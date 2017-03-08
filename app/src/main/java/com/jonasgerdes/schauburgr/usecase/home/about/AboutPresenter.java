c769d54b08136bf32686b5b286aae6bf1b608cc5
Jonas Gerdes <dev@jonasgerdes.com>
2017-03-09 00:10:09 +0100

    Add more licenses

--- app/src/main/java/com/jonasgerdes/schauburgr/usecase/home/about/AboutPresenter.java
+++ app/src/main/java/com/jonasgerdes/schauburgr/usecase/home/about/AboutPresenter.java
@@ -21,7 +21,28 @@ public class AboutPresenter implements AboutContract.Presenter {
         mView.setLicenses(
                 new OpenSourceLicense(
                         R.string.license_support_title,
-                        R.string.license_support_body)
+                        R.string.license_support_body),
+                new OpenSourceLicense(
+                        R.string.license_butterknife_title,
+                        R.string.license_butterknife_body),
+                new OpenSourceLicense(
+                        R.string.license_glide_title,
+                        R.string.license_glide_body),
+                new OpenSourceLicense(
+                        R.string.license_gson_title,
+                        R.string.license_gson_body),
+                new OpenSourceLicense(
+                        R.string.license_jodatime_title,
+                        R.string.license_jodatime_body),
+                new OpenSourceLicense(
+                        R.string.license_material_icons_title,
+                        R.string.license_material_icons_body),
+                new OpenSourceLicense(
+                        R.string.license_okhttp_title,
+                        R.string.license_okhttp_body),
+                new OpenSourceLicense(
+                        R.string.license_retrofit_title,
+                        R.string.license_retrofit_body)
         );
     }
 }
