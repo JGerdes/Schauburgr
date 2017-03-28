233ee59bcf68edab3290d0bde0615e294c6407d0
Jonas Gerdes <dev@jonasgerdes.com>
2017-03-28 21:11:00 +0200

    Fix days were added multiple times

--- app/src/main/java/com/jonasgerdes/schauburgr/model/ScreeningDay.java
+++ app/src/main/java/com/jonasgerdes/schauburgr/model/ScreeningDay.java
@@ -6,12 +6,16 @@ import java.util.Date;
 
 import io.realm.RealmList;
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 /**
  * Created by jonas on 05.03.2017.
  */
 
-public class ScreeningDay extends RealmObject{
+public class ScreeningDay extends RealmObject {
+    @PrimaryKey
+    private long id;
+
     private Date date;
     private RealmList<ScreeningTime> times = new RealmList<>();
 
@@ -21,6 +25,7 @@ public class ScreeningDay extends RealmObject{
 
     public ScreeningDay setDate(LocalDate date) {
         this.date = date.toDate();
+        this.id = this.date.getTime();
         return this;
     }
 
