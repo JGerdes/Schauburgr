4b0d3705f0010f46e084f91bf2ba641f36590f1c
Jonas Gerdes <dev@jonasgerdes.com>
2017-03-29 14:11:55 +0200

    Add splash screen

--- app/src/main/java/com/jonasgerdes/schauburgr/usecase/home/HomeActivity.java
+++ app/src/main/java/com/jonasgerdes/schauburgr/usecase/home/HomeActivity.java
@@ -30,6 +30,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
