6142d651d75bd5dd6165dc16abfdb8b1ff760369
Jonas Gerdes <dev@jonasgerdes.com>
2017-05-20 16:42:38 +0200

    Make status bar translucent when movie has a cover

--- app/src/main/java/com/jonasgerdes/schauburgr/usecase/movie_detail/MovieDetailActivity.java
+++ app/src/main/java/com/jonasgerdes/schauburgr/usecase/movie_detail/MovieDetailActivity.java
@@ -28,6 +28,7 @@ import android.transition.Slide;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -238,6 +239,11 @@ public class MovieDetailActivity extends AppCompatActivity
         if (movie.getCoverUrl() != null) {
             Glide.with(this)
                     .load(movie.getCoverUrl())
+                    .asBitmap()
+                    .listener(new GlideListener(bitmap -> {
+                        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                    }))
                     .into(mCoverView);
         }
     }
