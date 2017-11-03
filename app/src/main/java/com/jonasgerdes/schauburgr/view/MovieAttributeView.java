06931ad4d6288f16c9bd5d89a7e7e27647055eb2
Jonas Gerdes <dev@jonasgerdes.com>
2017-11-03 21:23:24 +0100

    Update gradle and support libs

--- app/src/main/java/com/jonasgerdes/schauburgr/view/MovieAttributeView.java
+++ app/src/main/java/com/jonasgerdes/schauburgr/view/MovieAttributeView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.ViewGroup;
@@ -81,7 +80,6 @@ public class MovieAttributeView extends LinearLayout {
             layout.gravity = Gravity.LEFT | Gravity.CENTER_VERTICAL;
             attributeView.setLayoutParams(layout);
             attributeView.setText(attribute);
-            Log.d("MovieAttrView", "setAttributes: " + horizontalMarginDp);
 
             if (Movie.EXTRA_LAST_SCREENINGS.equals(attributeName)) {
                 attributeView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 9);
